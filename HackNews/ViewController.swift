95f8a0b8343bd27dd2ee44702fd9def9c33aed43
guchan-fout <stevengc@gmail.com>
2023-12-01 20:35:32 +0900

    Initial Commit

---
+++ HackNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackNews
+//
+//  Created by Chan Gu on 2023/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
